d540998aa1645cf0ca4fd9899207c9986002bfb9
eelkiaer <84961075+eelkiaer@users.noreply.github.com>
2025-06-20 12:59:17 +0200

    Add Tikz functionality

--- site.hs
+++ site.hs
@@ -1,8 +1,30 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
+--import           Data.Monoid (mappend)
 import           Hakyll
 
+import Text.Pandoc.Definition
+import Text.Pandoc.Walk (walkM)
+import Control.Monad ((>=>))
+import Data.ByteString.Lazy.Char8 (pack, unpack)
+import qualified Network.URI.Encode as URI (encode)
+import qualified Data.Text as Text
+
+---------------------------------------------------------------------------------
+
+myPandocCompiler = pandocCompilerWithTransformM defaultHakyllReaderOptions defaultHakyllWriterOptions $ walkM tikzFilter
+
+tikzFilter :: Block -> Compiler Block
+tikzFilter (CodeBlock (id, "tikzpicture":extraClasses, namevals) contents) =
+  (imageBlock . ("data:image/svg+xml;utf8," ++) . URI.encode . filter (/= '\n') . itemBody <$>) $
+    makeItem (Text.unpack contents)
+     >>= loadAndApplyTemplate (fromFilePath "templates/tikz.tex") (bodyField "body")
+     >>= withItemBody (return . pack
+                       >=> unixFilterLBS "rubber-pipe" ["--pdf"]
+                       >=> unixFilterLBS "pdftocairo" ["-svg", "-", "-"]
+                       >=> return . unpack)
+  where imageBlock fname = Para [Image (id, "tikzpicture":extraClasses, namevals) [] (Text.pack fname, "")]
+tikzFilter x = return x
 
 --------------------------------------------------------------------------------
 config :: Configuration
@@ -66,7 +88,6 @@ main = hakyllWith config $ do
                 >>= relativizeUrls
 
     match "templates/*" $ compile templateBodyCompiler
-
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
